e7ce00561e1c55b656aeeefdf731d448cba6cc99
haakonaareskjold <aareskjold.hhaakon@gmail.com>
2021-03-23 03:59:47 +0100

    added example of notification pop up

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,6 @@
 let mix = require('laravel-mix')
 
-mix.sass('src/sass/styles.scss', 'assets')
+mix.js('src/app.js', 'dist')
+   .sass('src/sass/styles.scss', 'assets')
    .copy('src/index.html', 'dist')
    .setPublicPath('dist');
